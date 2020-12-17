a30666c0020046ec7d327baef7c7646765eda43d
zhaohoj <zhaohongjie@hualumedia.com>
2020-12-17 19:02:15 +0800

    init

--- submit.py
+++
@@ -1,9 +0,0 @@
-import shutil
-import pandas as pd
-
-# shutil.copy('input/out/submission.csv', 'submission.csv')
-xx = pd.read_csv('input/out/submission.csv')
-print(xx.shape)
-
-xx = pd.read_csv('input/submission.csv')
-print(xx.get(0))
